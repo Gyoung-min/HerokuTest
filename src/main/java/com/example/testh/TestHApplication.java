ad584a2e0225fa59f30df7bf63b53c641a46a7e0
Min <rudals0326@gmail.com>
2022-08-24 00:33:09 +0900

    first

---
+++ src/main/java/com/example/testh/TestHApplication.java
@@ -0,0 +1,13 @@
+package com.example.testh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testh/TestHApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
